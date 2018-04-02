a2b2dff5691a34e41608cf3d152e0892df310fdd
madhavparekh <madhavparekh@gmail.com>
2018-04-02 09:07:12 -0700

    started user prompt implementation

--- beHangman.js
+++ beHangman.js
@@ -1,7 +1,27 @@
-var Inquirer = require('inquirer');
+var inquirer = require('inquirer');
 var Char = require('./objects/Char');
 var Word = require('./objects/Word');
 
 var word = new Word();
-console.log(word.word);
-console.log(word.jumble);
+console.log(word.displayWord());
+
+inquirer.prompt([
+    {   type: 'confirm',
+        message: `Welcome to game of Pizza Themed Hangman where all words are related to pizzas and pizza's origin.\n  Press key a thru z or space bar to solve\n  Would you like to start?`,
+        name: 'start',
+    },
+    {
+        message: `Solve this: ${word.displayWord()}\n `,
+        name: 'char',
+        type: 'text',
+        validate: (char)=>{
+            if(/.[\w|\s]/.test(char))
+                return 'Enter single charactor';
+            if()
+
+        },
+    }
+
+]).then(()=>{
+
+});
